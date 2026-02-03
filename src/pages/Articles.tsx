05bc008b98d76e9c19de4f84a5b54e20f74aa213
Alexander Wu <96809548+Alexer-Eternity@users.noreply.github.com>
2026-02-03 13:57:22 +1100

    Added styling for resources page

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,8 +1,24 @@
-
+import articleData from './article.json'
 function Articles() {
     return (
-        <div></div>
-    )
+        <div className="articles-container">
+            <h1> Articles About Go</h1>
+            <ul className="articles-list">
+                {articleData.map((article,index) => (
+                <li key={index} className= "article-item">
+                <a href={article.url} className="article-link"target="_blank" rel="noopener noreferrer">
+                    {article.title}
+                </a>
+                <span className={"article-author"}> Author: {article.author}</span>
+                </li>
+
+
+                    ))}
+            </ul>
+
+
+
+        </div>);
 }
 
 export default Articles;
